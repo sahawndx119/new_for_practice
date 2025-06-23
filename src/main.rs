724d0303e23fdc1709ae659d55acb2e55c93b9ae
Sahand Salehi <sahawndx@gmail.com>
2025-06-24 02:12:26 +0330

    i added some commands in main.rs which get the city make a person type struct and prints every fields of it

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 // in the name of God 
 
+use practice::Person;
+
 fn main () {
     println!("hello please enter your name");
     let mut name = String::new();
     std::io::stdin().read_line(&mut name).unwrap();
 
-    println!("name >> {}" , name.trim());
 
     println!("please enter your age");
     let mut age = String::new();
     std::io::stdin().read_line(&mut age).unwrap();
     let age = age.trim().parse::<u32>().unwrap();
-    
 
-    println!("age >> {age}");
+
+    println!("please enter your city");
+    let mut city = String::new();
+    std::io::stdin().read_line(&mut city).unwrap();
+
+
+    let person = Person::new(name.trim(), age, city.trim());
+    println!("created a struct type named person");
+
+    println!("name >> {}" , person.name);
+    println!("age >> {}" , person.age);
+    println!("city >> {}" , person.city);
 }
