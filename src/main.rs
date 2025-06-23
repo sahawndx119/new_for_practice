e3b023d23583a772ae2e58b1214ba4f262e94d81
Sahand Salehi <sahawndx@gmail.com>
2025-06-23 17:00:22 +0330

    the main.rs get the name of the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+// in the name of God 
+
+fn main () {
+    println!("hello please enter your name");
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+
+    println!("name >> {}" , name.trim());
 }
